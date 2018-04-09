196dd6635b326d6597366ab9ce295e2ed353ad8b
wanglijun <>
2018-04-09 11:31:29 +0800

    重新提交

--- LJScroView-master/Pods/Target Support Files/Pods-LJScroView-master/Pods-LJScroView-master-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LJScroView_master : NSObject
-@end
-@implementation PodsDummy_Pods_LJScroView_master
-@end
--- LJScroView-master/Pods/Target Support Files/Pods-LJScroView-masterTests/Pods-LJScroView-masterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LJScroView_masterTests : NSObject
-@end
-@implementation PodsDummy_Pods_LJScroView_masterTests
-@end
